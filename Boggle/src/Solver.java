0d8a784a8289f03ba9d15804503c69a43dac93f8
A-L-M <al3cmills@gmail.com>
2018-04-14 13:13:49 -0600

    add comments

--- Boggle/src/Solver.java
+++ Boggle/src/Solver.java
@@ -1,12 +1,18 @@
-/*****************************************
- * Thought:
- * rename Space -> Node
- * TrieNode -> Trie
- * doing so should make the graph nature
- * of the boggle board more obvious
- * Also:
- * Comment your code numbnuts!
- ****************************************/
+/************************************************************
+ * Author: Alec Mills & Devon Lee
+ *
+ * Generates ArrayList of Strings that are:
+ * A) possible to be formed according to the rules of boggle
+ * B) present in the given word-list, represented by a trie
+ *
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *  Boggle Movement rules:
+ *  -no node may be used more than once per String
+ *  -beginning with the first node chose, each
+ *   subsequent node must be adjacent to the previous;
+ *   up/down,left/right & diagonal are valid directions
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ ************************************************************/ 
 
 import java.util.ArrayList;
 import java.util.Stack;
@@ -14,12 +20,14 @@ import java.util.Stack;
 public class Solver {
 
     public ArrayList<String> foundWords = new ArrayList<>();
-    private boolean[][] visited = new boolean[4][4];
+    //Stack tracks our position in both the board graph, an the trie
     Stack<Tuple> path = new Stack<>();
 
     public Solver() {
     }
 
+    //Tuple allows us to simultaneously track our position in the board graph
+    //as well as the trie
     class Tuple {
         private Node node;
         private Trie trie;
@@ -35,18 +43,29 @@ public class Solver {
         }
     }
 
-    //later make sure word is > 3 chars
+    //returns all legal-move valid strings beginning from one starting point
     public void solve(Node node, Trie root) {
-        Node child;
+        
+        Node child;//used to point to the next adjacent node to our current node
+
+        //first element on stack should always be a Tuple corresponding to
+        //the boardnode passed as an argument, and the trie's root-node's child
+        //corresponding to that board-node's letter value
         Tuple current = path.push(new Tuple(node, root.getChild(node.value)));
+        //when the stack pops off the final element, we know we have explored
+        //all possible paths as far as necessary
         while (!path.isEmpty()) {
+            //all Tuple operations in our loop will need to be performed on the
+            //top element of the stack
             current = path.peek();
             String word = current.trie.getWord();
             if (word != null && foundWords.indexOf(word) < 0) {
-                foundWords.add(word);
+                foundWords.add(word);//if not null, we know we have a valid word
             }
+            //check if current.index will refer to a valid child of current.node
             boolean inBounds = current.index >= 0 && 
                     current.index < current.node.allNeighbors().size();
+            //if not, we can terminate this search path
             if (!inBounds) {
                 path.pop();
                 continue;
@@ -55,21 +74,30 @@ public class Solver {
                 child = current.node.getNeighbor(current.index);
             }
 
+            //if it is, check if that child is already an elemnt of the stack
+            //in order to prevent infinite loops
             if (!inStack(child)) {
+                //if its not, but the corresponding trie has no children corresponding
+                //to the letter value, we know we can terminate that search path
                 if (!current.trie.hasChild(child.value)) {
                     path.pop();
                     continue;
                 }
 
+                //otherwise, move to that position on the board graph and the
+                //corresponding position on the trie
                 path.push(new Tuple(child, current.trie.getChild(child.value)));
 
             }
-
+            //before changing 'current to point to top stack element, iterate
+            //its index so that next time we operate on current, it will not
+            //repeat operations on an already visited child
             current.index++;
         }
 
     }
 
+    //returns true if node is already present in the stack
     private boolean inStack (Node node) {
         for (Tuple el : path) {
             if (el.node == node) return true;
