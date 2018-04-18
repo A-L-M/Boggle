3230a7641ec253ef0b79d143e9af4630dfe5b448
A-L-M <al3cmills@gmail.com>
2018-04-17 19:52:30 -0600

    fix

--- Boggle/src/Main.java
+++ Boggle/src/Main.java
@@ -4,26 +4,13 @@ import javax.swing.JFrame;
 
 public class Main {
     public static void main (String[] args) {
-        File dict = new File("../Dictionaries/yawl.txt.trim");
+        //File dict = new File("../Dictionaries/yawl.txt.trim");
+        File dict = new File("yawl.txt.trim");
         Board board = new Board(dict);
         board.print();
-        Solver solver = new Solver();
+        Solver solver = new Solver();//instantiate solver
         Trie root = new Trie(dict);//make our Trie out of a given dictionary file
         System.out.println();//new line for formatting
-
-        /*
-        System.out.println("Found words: ");
-        for (Node[] row : board.getBoard()) {
-            for (Node el : row) {
-                solver.solve(board.getNode(el), root);
-            }
-        }
-        for(String el : solver.foundWords) {
-            if(el != null) {
-                System.out.println(el);
-            }
-        }
-        */
 
         BoggleFrame game = new BoggleFrame(board);
         game.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
