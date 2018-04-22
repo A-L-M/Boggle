6ef7d06af8be237aa47019029836321adcaf3ef7
A-L-M <al3cmills@gmail.com>
2018-04-22 00:07:30 -0600

    fix merge conflicts

--- Boggle/src/Node.java
+++ Boggle/src/Node.java
@@ -17,31 +17,18 @@ class Node {
         this.row = row;
         this.col = col;
         this.value = value;
-<<<<<<< HEAD
         neighbors = new ArrayList<>();
-=======
-        neighbors = new ArrayList<>();            
->>>>>>> 58cfb6572160f6aeb7fdd61967192c227ddff4d0
     }
 
     public void addNeighbor(Node neighbor) {
         neighbors.add(neighbor);
-<<<<<<< HEAD
     }
-=======
-    } 
->>>>>>> 58cfb6572160f6aeb7fdd61967192c227ddff4d0
 
     public Node getNeighbor(int index) {
         boolean inBounds = index >= 0 && index < neighbors.size();
         if (inBounds) {
             return neighbors.get(index);
-<<<<<<< HEAD
         } else return null;
-=======
-        }
-        else return null;
->>>>>>> 58cfb6572160f6aeb7fdd61967192c227ddff4d0
 
     }
 
--- Boggle/src/TrimFile.java
+++ Boggle/src/TrimFile.java
@@ -1,9 +1,5 @@
 /*************************************************
-<<<<<<< HEAD
  * Author: Alec Mills & Devon Lee
-=======
- * Author: Alec Mills
->>>>>>> 58cfb6572160f6aeb7fdd61967192c227ddff4d0
  *
  * processes a text file of the form:
  * one word per line
@@ -11,18 +7,11 @@
  * and converts all strings to upper case
  ************************************************/
 
-<<<<<<< HEAD
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
-=======
-import java.util.Scanner;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
->>>>>>> 58cfb6572160f6aeb7fdd61967192c227ddff4d0
 
 public class TrimFile {
 
@@ -32,18 +21,10 @@ public class TrimFile {
         try {
             Scanner read = new Scanner(new File(fileName));
             File newFile = new File(fileName + ".trim");
-<<<<<<< HEAD
             FileWriter writer = new FileWriter(newFile);
 
             while (read.hasNextLine()) {
                 //upper case
-=======
-            newFile.createNewFile();
-            FileWriter writer = new FileWriter(newFile);
-
-            while(read.hasNextLine()) {
-                //upper case 
->>>>>>> 58cfb6572160f6aeb7fdd61967192c227ddff4d0
                 String next = read.nextLine();
                 if (next.length() >= minLength) {
                     writer.write(next.toUpperCase() + "\n");
@@ -55,7 +36,6 @@ public class TrimFile {
                 }
                 */
             }
-<<<<<<< HEAD
         } catch (FileNotFoundException e) {
             e.printStackTrace();
             System.out.println("File not found, please correct file path");
@@ -69,21 +49,6 @@ public class TrimFile {
         try {
             trim(args[0], Integer.parseInt(args[1]));
         } catch (ArrayIndexOutOfBoundsException e) {
-=======
-        }
-        catch (ArrayIndexOutOfBoundsException e) {
-            throw new IllegalArgumentException("please specify file path as argument");
-        }
-        catch (IOException e) {
-            System.out.println("file not found, please correct file path");
-        }
-    }
-    public static void main(String[] args) {
-        try {
-            trim(args[0], Integer.parseInt(args[1]));
-        }
-        catch (Exception e) {
->>>>>>> 58cfb6572160f6aeb7fdd61967192c227ddff4d0
             System.out.println("Please specify valid file path as argument");
         }
     }
